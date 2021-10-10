57bc97ae3087bb00bfbd651e93556f74c00f20b0
rodrigomsrocha <rodrigomtc82@gmail.com>
2021-10-10 19:06:23 -0300

    fix header style

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,12 +4,14 @@ import styles from './header.module.scss';
 
 export default function Header(): JSX.Element {
   return (
-    <header className={`${commonStyles.container} ${styles.header}`}>
-      <Link href="/">
-        <a>
-          <img src="/logo.svg" alt="logo" />
-        </a>
-      </Link>
-    </header>
+    <div className={commonStyles.container}>
+      <header className={styles.header}>
+        <Link href="/">
+          <a>
+            <img src="/logo.svg" alt="logo" />
+          </a>
+        </Link>
+      </header>
+    </div>
   );
 }
